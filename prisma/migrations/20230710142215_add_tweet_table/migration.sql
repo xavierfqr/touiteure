818e17b2f3d04c8860143eeb6c7dbe6a30185154
Etienne Guerlain <etienne.guerlain@matters.tech>
2023-07-10 16:26:46 +0200

    feat: #TECH-832 - create tweets table

---
+++ prisma/migrations/20230710142215_add_tweet_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Tweet" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "content" TEXT NOT NULL,
+    "authorId" TEXT NOT NULL,
+    "attachment" TEXT,
+
+    CONSTRAINT "Tweet_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Tweet" ADD CONSTRAINT "Tweet_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
