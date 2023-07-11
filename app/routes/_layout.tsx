bd2ed6d6d4d2a01c7503fec6fb53f4c78824671d
Xavier Facqueur <xavier.facqueur@matters.tech>
2023-07-11 15:36:08 +0200

    feat(TECH-843): use lucide-react icons

--- app/routes/_layout.tsx
+++ app/routes/_layout.tsx
@@ -3,20 +3,17 @@ import { useLoaderData, Link, Form, NavLink, Outlet } from "@remix-run/react";
 import { AbsoluteRoutes } from "../routes";
 import { getUserId } from "../session.server";
 
-import FeedIcon from "../../public/FeedIcon.svg";
-import LogoutIcon from "../../public/LogoutIcon.svg";
-import ProfileIcon from "../../public/ProfileIcon.svg";
-import TouiteureLogo from "../../public/TouiteureLogo.svg";
+import { Bird, LogOut, MessageSquare, User } from "lucide-react";
 
 const NOT_CONNECTED_TABS = [
-  { id: 1, name: "Login", url: AbsoluteRoutes.login },
-  { id: 2, name: "Sign up", url: AbsoluteRoutes.signup },
+  { name: "Login", url: AbsoluteRoutes.login },
+  { name: "Sign up", url: AbsoluteRoutes.signup },
 ];
 
 const TABS = [
-  { id: 1, name: "Feed", icon: FeedIcon, url: "." },
-  { id: 2, name: "Profile", icon: ProfileIcon, url: AbsoluteRoutes.profile },
-  { id: 3, name: "Logout", icon: LogoutIcon, url: AbsoluteRoutes.logout },
+  { name: "Feed", icon: MessageSquare, url: "." },
+  { name: "Profile", icon: User, url: AbsoluteRoutes.profile },
+  { name: "Logout", icon: LogOut, url: AbsoluteRoutes.logout },
 ];
 
 export const loader = async ({ request }: LoaderArgs) => {
@@ -33,12 +30,7 @@ export default function Layout() {
         <div className="h-full w-80 border-r bg-gray-50">
           <div className="my-8 flex justify-center">
             <Link to="." className="flex flex-col items-center justify-center">
-              <img
-                src={TouiteureLogo}
-                height={60}
-                width={60}
-                alt="touiteureIcon"
-              ></img>
+              <Bird height={60} width={60} />
               <i>Oizo</i>
             </Link>
           </div>
@@ -46,15 +38,16 @@ export default function Layout() {
           <ol>
             {userId
               ? TABS.map((tab) => {
+                  const Icon = tab.icon;
                   if (tab.url === AbsoluteRoutes.logout) {
                     return (
-                      <li key={tab.id}>
+                      <li key={tab.name}>
                         <Form action="/logout" method="post">
                           <button
                             type="submit"
                             className="block flex items-center gap-4 p-4 text-xl"
                           >
-                            <img src={tab.icon} width={20} alt={tab.name} />
+                            <Icon />
                             {tab.name}
                           </button>
                         </Form>
@@ -62,23 +55,23 @@ export default function Layout() {
                     );
                   }
                   return (
-                    <li key={tab.id}>
+                    <li key={tab.name}>
                       <NavLink
                         className={({ isActive }) =>
-                          `block flex gap-4 p-4 text-xl ${
+                          `block flex items-center gap-4 p-4 text-xl ${
                             isActive ? "bg-white text-blue-500" : ""
                           }`
                         }
                         to={tab.url}
                       >
-                        <img src={tab.icon} width={20} alt={tab.name} />
+                        <Icon />
                         {tab.name}
                       </NavLink>
                     </li>
                   );
                 })
               : NOT_CONNECTED_TABS.map((tab) => (
-                  <li key={tab.id}>
+                  <li key={tab.name}>
                     <NavLink
                       className={({ isActive }) =>
                         `block flex gap-4 p-4 text-xl ${
